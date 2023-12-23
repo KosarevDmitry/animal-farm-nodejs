f5c65ed6b452134381038635de151db7cac855ba
Dmitry Kosarev <kosarevdmlive@outlook.com>
2023-12-23 21:31:16 +0100

    #: update

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var port = process.env.PORT || 8080;
 var animals = {
     "cat": "meow",
     "dog": "bark",
+    "eel":"hiss",
+    "bear":"growl",
+    "frog":"croak",
+    "lion":"roar"
   
 }
 
